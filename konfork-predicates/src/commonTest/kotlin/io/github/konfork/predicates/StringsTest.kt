46a6576734b66ee7327fafa451e1248a9eabcb20
Geert Mulders <gmulders@gmail.com>
2022-11-18 13:00:46 +0100

    Add tests for allDigit

--- konfork-predicates/src/commonTest/kotlin/io/github/konfork/predicates/StringsTest.kt
+++ konfork-predicates/src/commonTest/kotlin/io/github/konfork/predicates/StringsTest.kt
@@ -1,5 +1,9 @@
 package io.github.konfork.predicates
 
+import io.github.konfork.predicates.cases.AllDigitCases
+import io.github.konfork.predicates.cases.EmailTestCases
+import io.github.konfork.predicates.cases.UuidTestCases
+import io.github.konfork.predicates.util.assert
 import kotlin.test.Test
 
 class StringsTest {
@@ -20,5 +24,11 @@ class StringsTest {
         assert(cases.validV3, cases.invalidV3, isUuidVersion(3))
         assert(cases.validV4, cases.invalidV4, isUuidVersion(4))
         assert(cases.validV5, cases.invalidV5, isUuidVersion(5))
+    }
+
+    @Test
+    fun isAllDigits() {
+        val cases = AllDigitCases()
+        assert(cases.valid, cases.invalid, ::isAllDigits)
     }
 }
