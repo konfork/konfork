7d1b3767e30c5ba88763a8879d172605a1409acd
Geert Mulders <gmulders@gmail.com>
2022-11-18 12:58:09 +0100

    Fix patterns for use in JS and remove unneeded start and end matchers

--- konfork-predicates/src/commonMain/kotlin/io/github/konfork/predicates/Strings.kt
+++ konfork-predicates/src/commonMain/kotlin/io/github/konfork/predicates/Strings.kt
@@ -3,7 +3,7 @@ package io.github.konfork.predicates
 import kotlin.text.RegexOption.IGNORE_CASE
 
 private val emailRegex = Regex(
-    "^(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|" +
+    "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|" +
             "\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\" +
             "[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]" +
             "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}" +
@@ -13,7 +13,7 @@ private val emailRegex = Regex(
 fun isEmail(s: String): Boolean = s.matches(emailRegex)
 
 private fun uuidRegexPattern(version: String): String =
-    "^[0-9A-F]{8}-[0-9A-F]{4}-[$version][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$"
+    "[0-9A-F]{8}-[0-9A-F]{4}-[$version][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}"
 private fun uuidRegex(version: Int): Regex = Regex(uuidRegexPattern(version.toString()), IGNORE_CASE)
 
 private const val nilUuidRegex = "00000000-0000-0000-0000-000000000000"
