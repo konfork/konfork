1b5adfce70d1fd379a5bb5fa461396d0d2dfe66f
Geert Mulders <gmulders@gmail.com>
2022-11-18 13:42:01 +0100

    Add EAN check

--- konfork-predicates/src/commonMain/kotlin/io/github/konfork/predicates/CheckDigits.kt
+++ konfork-predicates/src/commonMain/kotlin/io/github/konfork/predicates/CheckDigits.kt
@@ -4,6 +4,13 @@ fun isMod10(evenWeight: Int, oddWeight: Int) : (String) -> Boolean = {
     isMod10(it, evenWeight, oddWeight, Int::times)
 }
 
+fun isEan(length: Int): (String) -> Boolean {
+    val mod10Fn = isMod10(1, 3)
+    return {
+        it.length == length && mod10Fn(it)
+    }
+}
+
 private fun isMod10(s: String, evenWeight: Int, oddWeight: Int, map: (Int, Int) -> Int): Boolean =
     isAllDigits(s) && validateMod10Checksum(s, evenWeight, oddWeight, map)
 
--- konfork-predicates/src/commonTest/kotlin/io/github/konfork/predicates/CheckDigitsTests.kt
+++ konfork-predicates/src/commonTest/kotlin/io/github/konfork/predicates/CheckDigitsTests.kt
@@ -1,5 +1,6 @@
 package io.github.konfork.predicates
 
+import io.github.konfork.predicates.cases.EanCases
 import io.github.konfork.predicates.cases.Mod10Cases
 import io.github.konfork.predicates.util.assert
 import kotlin.test.Test
@@ -10,5 +11,15 @@ class CheckDigitsTests {
         val cases = Mod10Cases()
         assert(cases.valid13, cases.invalid13, isMod10(1, 3))
         assert(cases.valid12, cases.invalid12, isMod10(1, 2))
+    }
+
+    @Test
+    fun isEanTest() {
+        val cases = EanCases()
+        assert(cases.valid8, cases.invalid8, isEan(8))
+        assert(cases.valid12, cases.invalid12, isEan(12))
+        assert(cases.valid13, cases.invalid13, isEan(13))
+        assert(cases.valid14, cases.invalid14, isEan(14))
+        assert(cases.valid18, cases.invalid18, isEan(18))
     }
 }
